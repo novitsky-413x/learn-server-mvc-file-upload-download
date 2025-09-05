50530385a97a4aea1559717a0702e4ffadecf39c
Alexander Novitsky <novitsky.413x@gmail.com>
2025-09-05 12:36:34 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,87 @@
+require('dotenv').config();
+const mongoPass = process.env.MONGO_PASS;
+const mongoUser = process.env.MONGO_USER;
+const mongoAddr = process.env.MONGO_ADDR;
+const MONGODB_URI = `mongodb+srv://${mongoUser}:${mongoPass}@${mongoAddr}/shop`;
+// const MONGODB_URI = `mongodb+srv://${mongoUser}:${mongoPass}@${mongoAddr}/shop?retryWrites=true`;
+
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions',
+});
+const csrfProtection = csrf();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }));
+app.use(csrfProtection);
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
+app.use((req, res, next) => {
+    // throw new Error('Sync Dummy error');
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
+        .then((user) => {
+            // throw new Error('Async Dummy error');
+            if (!user) {
+                return next();
+            }
+            req.user = user;
+            next();
+        })
+        .catch((err) => {
+            next(new Error(err));
+            // inside async code, we need to call next() with an argument
+            // to pass the error to the error handling middleware
+        });
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+app.get('/500', errorController.get500);
+app.use(errorController.get404);
+app.use((error, req, res, next) => {
+    // res.redirect('/500');
+    res.status(500).render('500', {
+        pageTitle: 'Error!',
+        path: '/500',
+        isAuthenticated: req.session.isLoggedIn,
+    });
+});
+
+mongoose
+    .connect(MONGODB_URI)
+    .then((result) => {
+        app.listen(3000);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
